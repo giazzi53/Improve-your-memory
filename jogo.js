93b2f739bee26d291ab2791a9f880b7bafcdefb4
Guilherme Giazzi <32527872+giazzi53@users.noreply.github.com>
2018-10-01 22:48:42 -0300

    Desenvolvida lógica de limitar a escolha de cartas por pares

--- jogo.js
+++ jogo.js
@@ -3,14 +3,11 @@ userPoints = 0;
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
   
-    // While there remain elements to shuffle...
     while (0 !== currentIndex) {
   
-      // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
   
-      // And swap it with the current element.
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
@@ -23,51 +20,58 @@ cards = ["images/queda-bastilha.png", "images/diretas-ja.png", "images/homem-lua
          "images/steve-jobs.png", "images/hitler.png", "images/queda-bastilha.png", 
          "images/diretas-ja.png", "images/homem-lua.png",
          "images/steve-jobs.png", "images/hitler.png"];
+
 index = [0,1,2,3,4,5,6,7,8,9];
 randomIndex = shuffle(index);
-
+equalCards = false;
 clicks = 0;
 selectedCards = [];
-
+auxClickedCards = [];
 function revealCard(num){
     clicks++;
     document.getElementsByClassName("hidden-card")[num].src = cards[randomIndex[num]]; 
     selectedCard = cards[randomIndex[num]];
     selectedCards.push(selectedCard);
+    auxClickedCards.push(num);
     if(clicks == 2){
-        checkCards(selectedCards[0], selectedCards[1]);
-        clicks = 0;
-        selectedCards = [];
-    }
+    	///!!!
+        window.alert("Concluiu uma jogada"); //o bug está aqui, mas pq?
+        ///!!!
+        if(auxClickedCards[0] != auxClickedCards[1]){
+            checkCards(selectedCards[0], selectedCards[1]);
+        
+            if(equalCards == true){ //tira o par de cartas da tela se for igual
+                document.getElementsByClassName("hidden-card")[auxClickedCards[0]].src = "";
+                document.getElementsByClassName("hidden-card")[auxClickedCards[1]].src = "";
+            } else{ //volta as duas cartas para baixo
+                document.getElementsByClassName("hidden-card")[auxClickedCards[0]].src = "images/hidden-card.png";
+                document.getElementsByClassName("hidden-card")[auxClickedCards[1]].src = "images/hidden-card.png";
+            }
+
+            clicks = 0;
+            selectedCards = [];
+            auxClickedCards = [];
+
+        } else{
+            auxClickedCards.pop();
+            selectedCards.pop();
+            alert("Nao pode clicar na mesma carta");
+            clicks--;
+            
+        }
+    } 
 }
 
 function checkCards(card1, card2){ //funcao que verifica se as cartas viradas sao iguais
     if(card1 == card2){
         userPoints ++;
+        equalCards = true;
+    } else{
+        equalCards = false;
     }
 
-    window.alert("Pontuação:" + userPoints);
+    
 }
 
-var modal = document.getElementById('simpleModal');
-var modalBtn = document.getElementById('modalBtn');
-var closeBtn = document.getElementsByClassName('closeBtn')[0];
 
-modalBtn.addEventListener('click', openModal);
-closeBtn.addEventListener('click', closeModal);
-window.addEventListener('click', outsideClick);
-
-function openModal(){
-  modal.style.display = 'block';
-}
-
-function closeModal(){
-  modal.style.display = 'none';
-}
-
-function outsideClick(e){
-  if(e.target == modal){
-    modal.style.display = 'none';
-  }
-}
 
