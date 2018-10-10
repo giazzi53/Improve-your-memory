332c1dd7054e4a435a91ec71c49498a03475eb63
andrevinicius201 <32529232+andrevinicius201@users.noreply.github.com>
2018-10-10 13:55:26 -0300

    Add files via upload
    adicionando o arquivo para manipular a fonte e dar acessibilidade

---
+++ js/acessibilidade.js
@@ -0,0 +1,8 @@
+var texto = document.querySelectorAll('h1,h2,h3,h4,h5,h6,p,span');
+
+               function aumentaFonte(){
+                   alert("testando");
+                	for(i=0; i< texto.length; i++){
+                	texto[i].style.fontSize = "x-large"
+                	}
+                }
