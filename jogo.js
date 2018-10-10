2650d8b0e0d43220b8efe0f94584fe5d9ff45d76
Joao Costa <joao.costa@ARYZTA.COM>
2018-10-10 17:00:17 -0300

    Mudanças no evento de clique na carta

--- jogo.js
+++ jogo.js
@@ -1,115 +1,113 @@
 function shuffle(array) { //funcao de embaralhar as cartas
-    var currentIndex = array.length, temporaryValue, randomIndex;
-  
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
     while (0 !== currentIndex) {
-  
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
-  
+
     return array;
 }
 
-function Card(id, name){ //classe Card
+function Card(id, name) { //classe Card
     this.id = id;
     this.name = name;
     //this.position = position;
-    this.getId = function(){
+    this.getId = function () {
         return this.id;
     };
-    this.getName = function(){
+    this.getName = function () {
         return this.name;
     };
-    /*this.getPosition = function(){
-        return this.position;
-    };
-    this.getInfo = function(){
-        return this.id + this.name + this.position;
-    };*/
 }
 
 cards = ["images/queda-bastilha.png", "images/diretas-ja.png", "images/homem-lua.png",
-         "images/steve-jobs.png", "images/hitler.png", "images/queda-bastilha.png", 
-         "images/diretas-ja.png", "images/homem-lua.png",
-         "images/steve-jobs.png", "images/hitler.png"];
-userPoints = 0;
-cards = shuffle(cards);
-cardsObject = [];
-clicks = 0;
-selectedCards = [];
-for(a = 0; a < 10; a++){ //criando objetos card
-    var card = new Card(a, cards[a]);
-    cardsObject.push(card);
-}
+    "images/steve-jobs.png", "images/hitler.png", "images/queda-bastilha.png",
+    "images/diretas-ja.png", "images/homem-lua.png",
+    "images/steve-jobs.png", "images/hitler.png"
+];
 
-function findCard(id){ //encontrando a carta selecionada
-    for(card of cardsObject){
-        if(id == card.getId()){
+
+function findCard(id) { //encontrando a carta selecionada
+    for (card of cardsObject) {
+        if (id == card.getId()) {
             return card;
         }
     }
 }
 
-function revealCard(num){
-    clicks++;
-    selectedCard = findCard(num);
-     alert("Antes");
-    document.getElementById(num).src = selectedCard.getName();
-    alert("Depois");
+//objeto TURNO que vai guardar o "estado" do turno atual
+var turno = {
+    primeiro:{
+        elementoClicado : "",
+        cartaCorrespondente: ""
+    },
+    segundo:{
+        elementoClicado: "",
+        cartaCorrespondente: ""
+    },
+}
+var vezes = 0 //numero de "clicks"
+var pontos = 0 
 
-    //desedenvolver forma de comparar as duas cartas sem que seja por uma lista
-    selectedCards.push(selectedCard);
+$(document).ready(function () {
+    cards = shuffle(cards); //embaralha os cards
+    domCardsClone = []; //cria uma lista vazia
 
-    if(clicks == 2){
-        checkPair(selectedCards[0], selectedCards[1]);
+    //itera sobre cada um dos elementos que tem a classe "hidden-card" e adiciona
+    //uma copia desse elemento para a lista "domCardsClone"
+    $(".hidden-card").each(function () {
+        domCardsClone.push(this.cloneNode())
+    });
+
+    //para cada copia do node na lista, adiciona o caminho 
+    //que esta em "cards"
+    for (var i = 0; i < domCardsClone.length; i++) {
+        domCardsClone[i].src = cards[i]
     }
 
-}
 
-function checkPair(card1, card2){
-    if(card1.getName() == card2.getName() && card1.getId() != card2.getId()){ //se a primeira carta for igual
-        clicks = 0;                                                           //a segunda e nao for a mesma carta
-        userPoints++;   
-        selectedCards = [];
-        document.getElementById(card1.getId()).src = ""; 
-        document.getElementById(card2.getId()).src = "";                                                 
-        alert("cartas iguais");
-    } else if(card1.getName() == card2.getName() && card1.getId() == card2.getId()){ //se a primeira carta for igual
-        clicks--;                                                                    //a segunda, mas for a mesma carta
-        selectedCards.pop();
-        alert("nao pode clicar na mesma carta");
-    } else{ //se as cartas forem diferentes
-        clicks = 0;                                                           
-        selectedCards = [];
-        document.getElementById(card1.getId()).src = "images/hidden-card.png"; 
-        document.getElementById(card2.getId()).src = "images/hidden-card.png";       
-        alert("cartas diferentes");
-    }
-}
+    //para cada click no elemento "hidden-card"
+    $('.hidden-card').click(function () {
+        //verifica o numero da vez
+        if (vezes == 0) {
+            vezes++ //incrementa o numero de vezes
+
+            //adiciona o elemento <img> para o atributo "elementoclicado" do
+            //objeto "primeiro" que faz parte de "turno"
+            turno.primeiro.elementoClicado = this
+            turno.primeiro.cartaCorrespondente = cards[this.id]
+            this.src = turno.primeiro.cartaCorrespondente
+
+        } else if (vezes == 1) {
+            vezes++
+            turno.segundo.elementoClicado = this
+            turno.segundo.cartaCorrespondente = cards[this.id]
+            this.src = turno.segundo.cartaCorrespondente
 
 
-var modal = document.getElementById('simpleModal');
-var modalBtn = document.getElementById('modalBtn');
-var closeBtn = document.getElementsByClassName('closeBtn')[0];
-
-modalBtn.addEventListener('click', openModal);
-closeBtn.addEventListener('click', closeModal);
-window.addEventListener('click', outsideClick);
-
-function openModal(){
-  modal.style.display = 'block';
-}
-
-function closeModal(){
-  modal.style.display = 'none';
-}
-
-function outsideClick(e){
-  if(e.target == modal){
-    modal.style.display = 'none';
-  }
-}
+            //usa a função setTimeout, que faz com que o codigo so execute
+            //apos o numero de milisegundos definido
+            setTimeout(function(){
+                 if(turno.primeiro.cartaCorrespondente != turno.segundo.cartaCorrespondente){
+                     turno.primeiro.elementoClicado.src = 'images/hidden-card.png'
+                     turno.segundo.elementoClicado.src = 'images/hidden-card.png'
+                 }else if(turno.primeiro.cartaCorrespondente == turno.segundo.cartaCorrespondente){
+                    console.log('igual')
+                    pontos++
+                    document.getElementById('pontos').innerHTML = pontos
+                 }
+            }, 2000);
+        console.log("antes do if final", vezes)    
+        }if(vezes == 2){
+            //reseta o numero de vezes ao final
+            vezes = 0
+        }
+    });
+});
